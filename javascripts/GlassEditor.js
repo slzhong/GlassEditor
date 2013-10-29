e81854bb18adc9a4425b1c615a052ebbd6f6c2eb
Sherlock <sherlock.z0917@gmail.com>
2013-10-29 15:03:23 +0800

    almost finished

--- javascripts/GlassEditor.js
+++ javascripts/GlassEditor.js
@@ -1,6 +1,10 @@
 var GlassEditor = {
 	init : function(){
+		GlassEditor.styleHandler();
 		GlassEditor.eventHandler();
+	}
+	,styleHandler : function(){
+		GlassEditor.resizeCardContainer();
 	}
 	,eventHandler : function(){
 		// $('.preview').editable();
@@ -13,31 +17,108 @@ var GlassEditor = {
 		$('.editor-template-btn').click(
 			GlassEditor.showTemplates()
 		);
+		$('.editor-single-btn').click(
+			GlassEditor.singleMode()
+		);
+		$('.editor-multi-btn').click(
+			GlassEditor.multiMode()
+		);
+		$('.card-container').click(
+			GlassEditor.showCard()
+		);
+		$('.cards-add').click(
+			GlassEditor.addCard()
+		);
 		$('.editor-clear-btn').click(
 			GlassEditor.clearEditor()
 		);
+		$('.editor-export-btn').click(
+			GlassEditor.exportCard()
+		);
 		$('.editor-add-btn').click(
-			GlassEditor.addElement()
+			GlassEditor.insertElement()
+		);
+		$('.editor-class-type').click(
+			GlassEditor.showClasses()
+		);
+		$('.editor-class-item').click(
+			GlassEditor.insertClass()
+		);
+		$('.editor-add-style').click(
+			GlassEditor.addStyleTag()
 		);
 		$('.template').click(
 			GlassEditor.applyTemplate()
 		);
 	}
+	,resizeCardContainer : function(){
+		$('.cards-container').css('width', $('.cards-container').children().length * 180 + 'px');
+	}
 	,applyChanges : function(){
 		return function(){
 			$('#preview').html($('#editor').val());
+			if($('#preview').hasClass('preview-multi')){
+				$('.card-container-current').children('.card').html($('#editor').val());
+			}
 		}
 	}
 	,showTemplates : function(){
 		return function(){
-			$('.template-container').addClass('template-container-show');
+			if($('.template-container').hasClass('template-container-show')){
+				$('.template-container').removeClass('template-container-show');
+			}
+			else{
+				$('.template-container').addClass('template-container-show');
+			}
 		}
 	}
 	,applyTemplate : function(){
 		return function(){
 			$('.template-container').removeClass('template-container-show');
-			$('#preview').html($(this).html());
 			$('#editor').val($(this).html());
+			(GlassEditor.applyChanges())();
+		}
+	}
+	,singleMode : function(){
+		return function(){
+			if($('#preview').hasClass('preview-multi')){
+				$('#preview').removeClass('preview-multi');
+				$('#preview').addClass('preview-single');
+			}
+		}
+	}
+	,multiMode : function(){
+		return function(){
+			if($('#preview').hasClass('preview-single')){
+				$('#preview').removeClass('preview-single');
+				$('#preview').addClass('preview-multi');
+				$('.card').html($('#preview').html());
+			}
+		}
+	}
+	,showCard : function(){
+		return function(){
+			$('.card-container-current').removeClass('card-container-current');
+			$(this).addClass('card-container-current');
+			$('#preview').html($(this).children('.card').html());
+			$('#editor').val($(this).children('.card').html());
+		}
+	}
+	,addCard : function(){
+		return function(){
+			var id = 0;
+			var temp = $('.card-container-last');
+			var next = temp.clone();
+			$('.card-container-current').removeClass('card-container-current');
+			id = parseInt(temp.children('.card').attr('data-cardID'));
+			temp.removeClass('card-container-last');
+			next.addClass('card-container-current');
+			next.insertAfter(temp);
+			next.children('.card').html('').attr('data-cardID', id + 1);
+			next.click(GlassEditor.showCard());
+			GlassEditor.resizeCardContainer();
+			$('#editor').val(next.children('.card').html());
+			$('#preview').html(next.children('.card').html());
 		}
 	}
 	,clearEditor : function(){
@@ -46,7 +127,7 @@ var GlassEditor = {
 			$('#preview').html('');
 		}
 	}
-	,addElement : function(){
+	,insertElement : function(){
 		return function(){
 			var result = '';
 			var cursorMove = 0;
@@ -54,26 +135,26 @@ var GlassEditor = {
 			var end = $('#editor')[0].selectionEnd;
 			var type = $(this).attr('title');
 			if(type == 'table'){
-				result = '\n<table>\n<tbody></tbody>\n</table>\n';
-				cursorMove = 16;
+				result = '<table class="">\n<tbody></tbody>\n</table>\n';
+				cursorMove = 24;
 			}
 			else if(type == 'img'){
-				result = '\n<img src="">\n';
-				cursorMove = 11;
+				result = '<img src="">';
+				cursorMove = 10;
 			}
 			else if(type == 'li' || type == 'tr' || type == 'td'){
 				result = '<' + type +'></' + type + '>';
 				cursorMove = 4;
 			}
 			else{
-				result = '\n<' + type + '></' + type + '>\n';
-				cursorMove = 3 + type.length;
+				result = '<' + type + ' class=""></' + type + '>\n';
+				cursorMove = 11 + type.length;
 			}
 			if($('#editor').val() == ''){
 				$('#editor').val(
-					'<article>' + result + '</article>'
+					'<article class="">\n' + result + '</article>'
 				);
-				cursorMove += 9;
+				cursorMove += 19;
 			}
 			else{
 				$('#editor').val(
@@ -82,12 +163,48 @@ var GlassEditor = {
 			}
 			$('#editor').focus();
 			$('#editor')[0].selectionStart = $('#editor')[0].selectionEnd = start + cursorMove;
-			$('#preview').html($('#editor').val());
+			(GlassEditor.applyChanges())();
+		}
+	}
+	,addStyleTag : function(){
+		return function(){
+			$('#editor').val($('#editor').val() + '\n<style>\n\n</style>');
+			$('#editor')[0].selectionStart = $('#editor')[0].selectionEnd = $('#editor').val().length - 9;
 		}
 	}
 	,editInPlace : function(){
 		return function(){
-			alert('fuck');
+			
+		}
+	}
+	,showClasses : function(){
+		return function(){
+			if($(this).siblings('.editor-class-content').css('display') == 'block'){
+				$(this).siblings('.editor-class-content').slideUp(300);
+			}
+			else{
+				$(this).siblings('.editor-class-content').slideDown(300);
+			}
+		}
+	}
+	,insertClass : function(){
+		return function(){
+			var result = '';
+			var cursorMove = 0;
+			var start = $('#editor')[0].selectionStart;
+			var end = $('#editor')[0].selectionEnd;
+			result = $('#editor').val().substr(0, start) + $(this).html()  + ' ' + $('#editor').val().substr(end);
+			cursorMove = $(this).html().length + 1;
+			$('#editor').val(result);
+			$('#editor').focus();
+			$('#editor')[0].selectionStart = $('#editor')[0].selectionEnd = start + cursorMove;
+			$(this).parent().parent().slideUp(300);
+			(GlassEditor.applyChanges())();
+		}
+	}
+	,exportCard : function(){
+		return function(){
+			alert(JSON.stringify($('#editor').val()));
 		}
 	}
 }
